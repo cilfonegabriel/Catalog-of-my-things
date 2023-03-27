dac56d49143978c5fff7c2f3772fe8798623c133
gabicilfone <cilfonegabriel@gmail.com>
2023-03-27 18:52:11 -0300

    add methos for class item

--- lib/item.rb
+++ lib/item.rb
@@ -1,2 +1,24 @@
+create item class
 class Item
+  def initialize(publish_date:)
+    @id = SecureRandom.hex(10)
+    @publish_date = publish_date
+    @archived = false
+    @source = nil
+  end
+
+  attr_accessor :genre, :author, :label
+  attr_reader :id, :publish_date, :archived 
+
+
+  def can_be_archived?
+    return true if Date.strptime(publish_date, '%d/%m/%Y') < Date.today - 3652
+    false
+  end
+
+  def move_to_archive
+    return unless can_be_archived?
+
+    @archived = true
+  end
 end
