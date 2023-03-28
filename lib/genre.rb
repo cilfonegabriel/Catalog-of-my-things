5af4de00be60fc98c74671969dd6438d3a25a834
gabicilfone <cilfonegabriel@gmail.com>
2023-03-28 15:45:35 -0300

    add atribute accessor in genre file

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,6 +2,9 @@ require 'securerandom'
 
 class Genre 
 
+    attr_accessor :name, :items
+    attr_reader :id
+
     def initialize(name:, id = SecureRandom.uuid)
         @id = id
         @name = name
