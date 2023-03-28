5b09132f044979874b2a422806c8d545d2a6f594
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-28 10:20:57 -0500

    Create author.rb

---
+++ lib/author.rb
@@ -0,0 +1,22 @@
+require 'securerandom'
+require_relative './item'
+
+class Author
+  attr_accessor :id, :first_name, :last_name, :items
+
+  def initialize(first_name, last_name)
+    @id = SecureRandom.uuid
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item) unless @items.include?(item)
+    item.author = self
+  end
+end
+
+# @im-20230328 test temp - borrar
+# a = Author.new("Gabriel", "Garcia Marquez")
+# p a
