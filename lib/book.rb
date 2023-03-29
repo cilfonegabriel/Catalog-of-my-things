add33fa41ccc190057a199e233e5945b6882cc34
HunterX-7 <fraquin7@gmail.com>
2023-03-28 22:54:33 -0500

    Fix linters

--- lib/book.rb
+++ lib/book.rb
@@ -1,15 +1,15 @@
 require_relative 'item'
 
 class Book < Item
-    attr_accessor :publisher, :cover_state
+  attr_accessor :publisher, :cover_state
 
-    def initialize(*args, publisher, cover_state)
-        super(*args)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
+  def initialize(*args, publisher, cover_state)
+    super(*args)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
-    def can_be_archived?
-        super || @cover_state == 'bad'
-    end
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
 end
