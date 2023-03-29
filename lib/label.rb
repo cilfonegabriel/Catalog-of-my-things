80688639eaba8138e893dbee7800a89426bd4365
HunterX-7 <fraquin7@gmail.com>
2023-03-28 22:52:25 -0500

    Add label class with specified requirements

---
+++ lib/label.rb
@@ -0,0 +1,16 @@
+require 'securerandom'
+
+class label
+    attr_accessor :title, :color
+
+    def initialize(title, color)
+        @id = SecureRandom.uuid
+        @title = title
+        @color = color
+    end
+
+    def add_item(item)
+        @item << item
+        item.label << self
+    end
+end
