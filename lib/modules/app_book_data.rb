ec6583b7e9eebd110d51f6f7dbe44fa0e3e05d9b
HunterX-7 <fraquin7@gmail.com>
2023-03-30 17:45:42 -0500

    Fix linters

--- lib/modules/app_book_data.rb
+++ lib/modules/app_book_data.rb
@@ -5,14 +5,14 @@ module BookData
     @labels = []
     books_data.read.map do |book|
       new_book = Book.new(
-            publish_date: book['publish_date'], 
-            publisher: book['publisher'],
-            cover_state: book['cover_state']
-        )
+        publish_date: book['publish_date'],
+        publisher: book['publisher'],
+        cover_state: book['cover_state']
+      )
       label = Label.new(
-            title: book['label']['title'], 
-            color: book['label']['color']
-        )
+        title: book['label']['title'],
+        color: book['label']['color']
+      )
       label.add_item(new_book)
       @labels.push(label)
       @books.push(new_book)
