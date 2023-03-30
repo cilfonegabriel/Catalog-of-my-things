0150566196e5143053341f99005cfe7b656c4573
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-30 06:43:23 -0500

    Update game_modules - add valid_date? - format lists output

--- lib/modules/game_modules.rb
+++ lib/modules/game_modules.rb
@@ -22,7 +22,6 @@ module GameModule
       game_json.push({
                        id: game.id,
                        archived: game.archived,
-                       source: game.source,
                        publish_date: game.publish_date,
                        last_played_at: game.last_played_at,
                        multiplayer: game.multiplayer,
@@ -38,12 +37,39 @@ module GameModule
 
   def add_game
     puts '---- Create a game -----'
-    puts 'Is it muliplayer ? (yes or no):'
-    multiplayer = gets.chomp
-    puts 'Published Date (YYYY-MM-DD):'
-    publish_date = gets.chomp
-    puts 'Last played date (YYYY-MM-DD):'
-    last_played_at = gets.chomp
+    multiplayer=""
+    loop do
+      puts 'Is it muliplayer ? (yes, no, y, n):'
+      multiplayer = gets.chomp.downcase
+      if ['yes', 'no','y' ,'n'].include? multiplayer
+        break
+      end  
+    end
+
+    publish_date = ''
+    men = ''
+    loop do
+      puts men + 'Published Date (YYYY-MM-DD):'
+      publish_date = gets.chomp
+      if valid_date? (publish_date) 
+        puts '... ok a valid date'
+        break
+      end  
+      men = '>>> Enter a valid date >>> - '
+    end
+
+    men = ''
+    last_played_at = ''
+    loop do
+      puts men + 'Last played date (YYYY-MM-DD):'
+      last_played_at = gets.chomp
+      if valid_date? (last_played_at)
+        puts '... ok a valid date'
+        break
+      end         
+      men = '>>> Enter a valid date >>> '
+    end
+
     game_instance = Game.new(publish_date: publish_date, multiplayer: multiplayer, last_played_at: last_played_at)
     puts 'Game Author: '
     puts 'Author First Name: '
@@ -62,13 +88,12 @@ module GameModule
     if @games.empty?
       puts '>>> There aren\'t games'
     else
-      puts "\t\tList of Games:"
+      puts "\tList of Games"
       puts '--------------------------------------------------------'
       @games.each_with_index do |game, index|
-        puts " #{index + 1}
-          Publish Date: #{game.publish_date}
-          Last Played: #{game.last_played_at}
-          Author Name: #{game.author.first_name} #{game.author.last_name}"
+        print " #{index + 1}  - Publish Date: #{game.publish_date}" 
+        print "\n    - Last Played: #{game.last_played_at}" 
+        print "\n    - Author Name: #{game.author.first_name} #{game.author.last_name}\n"
       end
     end
     puts '=========================================================='
@@ -79,13 +104,21 @@ module GameModule
     if @authors.empty?
       puts '>>> There aren\'t Authors'
     else
-      puts "\t\tList of Authors:"
+      puts "\tList of Authors:"
       puts '--------------------------------------------------------'
       @authors.each_with_index do |author, index|
-        puts "#{index + 1}
-          Name: #{author.first_name}, #{author.last_name}"
+        puts "#{index + 1} - Name: #{author.first_name}, #{author.last_name}"
       end
     end
     puts '=========================================================='
+  end
+
+  def valid_date?(date)
+    # date_format = '%d-%m-%Y'
+    date_format = '%Y-%m-%d'
+    DateTime.strptime(date, date_format)
+    true
+  rescue ArgumentError
+    false
   end
 end
