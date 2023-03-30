c69dc8568d300ba6013c90e6969c736cc22dc89c
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-30 04:42:09 -0500

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,15 @@
 require 'date'
+require 'securerandom'
 require_relative './item'
+require_relative './author'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at, :publish_date
+  attr_accessor :multiplayer, :last_played_at
 
-  def initialize(genre, author, label, publish_date, last_played_at)
-    super(genre, author, label, publish_date)
-    @last_played_at = Date.parse(last_played_at)
+  def initialize(publish_date:, multiplayer:, last_played_at: Date.today)
+    super(publish_date: publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
   end
 
   private
