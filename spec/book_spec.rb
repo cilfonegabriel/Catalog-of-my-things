768d4708e7115f37ba9941e7766ae49539644306
HunterX-7 <fraquin7@gmail.com>
2023-03-30 17:45:51 -0500

    Fix linters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,20 +2,20 @@ require_relative '../lib/book'
 require_relative '../lib/item'
 
 describe Book do
-    context 'Test the Book class' do
-        it 'Create an instance object of the class book' do
-            book_one = Book.new(publisher: 'Sony', cover_state: 'good', publish_date: '2023-03-30')
-            expect(book_one).to be_instance_of(Book)
-        end
-
-        it 'Check if it can be archived case: true' do
-            book_two = Book.new(publisher: 'JK Books', cover_state: 'bad', publish_date: '1990-12-25')
-            expect(book_two.can_be_archived?).to be true
-        end
-
-        it 'Check if it can be archived case: false' do
-            book_three = Book.new(publisher: 'McKallen', cover_state: 'fair', publish_date: '2015-06-06')
-            expect(book_three.can_be_archived?).to be false
-        end
+  context 'Test the Book class' do
+    it 'Create an instance object of the class book' do
+      book_one = Book.new(publisher: 'Sony', cover_state: 'good', publish_date: '2023-03-30')
+      expect(book_one).to be_instance_of(Book)
     end
+
+    it 'Check if it can be archived case: true' do
+      book_two = Book.new(publisher: 'JK Books', cover_state: 'bad', publish_date: '1990-12-25')
+      expect(book_two.can_be_archived?).to be true
+    end
+
+    it 'Check if it can be archived case: false' do
+      book_three = Book.new(publisher: 'McKallen', cover_state: 'fair', publish_date: '2015-06-06')
+      expect(book_three.can_be_archived?).to be false
+    end
+  end
 end
