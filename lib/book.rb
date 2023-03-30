b1701b485eb61aae0720813c4fd1dbcb51b9e56d
HunterX-7 <fraquin7@gmail.com>
2023-03-30 12:50:00 -0500

    Update the args to be called correctly when init

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,11 @@
 require_relative 'item'
+require_relative 'label'
 
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(*args, publisher, cover_state)
-    super(*args)
+  def initialize(publish_date:, publisher:, cover_state:)
+    super(publish_date: publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
