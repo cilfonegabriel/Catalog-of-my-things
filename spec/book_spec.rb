7c40fb4692a2bfa3e28ed3eab34c28965ea97a16
HunterX-7 <fraquin7@gmail.com>
2023-03-30 17:27:38 -0500

    Add checj if it can be archived test1

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,5 +7,10 @@ describe Book do
             book_one = Book.new(publisher: 'Sony', cover_state: 'good', publish_date: '2023-03-30')
             expect(book_one).to be_instance_of(Book)
         end
+
+        it 'Check if it can be archived case: true' do
+            book_two = Book.new(publisher: 'JK Books', cover_state: 'bad', publish_date: '1990-12-25')
+            expect(book_two.can_be_archived?).to be true
+        end
     end
 end
