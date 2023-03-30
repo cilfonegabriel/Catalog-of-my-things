ab9815b707ec7a6abe7df25413659f939ef6b2ec
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-30 04:43:09 -0500

    Update app.rb - include GameModule

--- lib/app.rb
+++ lib/app.rb
@@ -3,14 +3,21 @@ require_relative './modules/music_album'
 require_relative './music_album'
 require_relative './genre'
 
+require_relative './modules/game_modules'
+require_relative './game'
+require_relative './author'
+
 class App
   include MusicAlbumModule
+  include GameModule
 
   def initialize
     load_music_albums
+    load_games
   end
 
   def save_files
     save_music_albums(@music_albums)
+    save_game(@games)
   end
 end
