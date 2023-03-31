dcf0cb76037a52344388ce39c9bc5aec0b0cadd9
gabicilfone <cilfonegabriel@gmail.com>
2023-03-30 21:02:14 -0300

    spec test

--- spec/musica_album_spec.rb
+++ spec/musica_album_spec.rb
@@ -3,10 +3,11 @@ require_relative '../lib/genre'
 require 'date'
 
 describe 'Music Album class test cases.' do
-  context 'Should take three parameters.' do
-    it 'Returns a MusicAlbum object' do
-      album = MusicAlbum.new(publish_date: '2023-03-21', on_spotify: true)
-      expect(album).to be_an_instance_of MusicAlbum
-    end
-  end
+    context 'Three parameters will be assigned.' do
+        it 'Will return an object' do
+          album = MusicAlbum.new(publish_date: '2022-04-10', on_spotify: true)
+          expect(album).to be_an_instance_of MusicAlbum
+        end
+     end
 end
+
