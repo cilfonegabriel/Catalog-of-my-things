3f3bd17993185377c701551c48e7ca0970849684
gabicilfone <cilfonegabriel@gmail.com>
2023-03-30 21:05:15 -0300

    fix linter

--- spec/musica_album_spec.rb
+++ spec/musica_album_spec.rb
@@ -3,11 +3,10 @@ require_relative '../lib/genre'
 require 'date'
 
 describe 'Music Album class test cases.' do
-    context 'Three parameters will be assigned.' do
-        it 'Will return an object' do
-          album = MusicAlbum.new(publish_date: '2022-04-10', on_spotify: true)
-          expect(album).to be_an_instance_of MusicAlbum
-        end
-     end
+  context 'Three parameters will be assigned.' do
+    it 'Will return an object' do
+      album = MusicAlbum.new(publish_date: '2022-04-10', on_spotify: true)
+      expect(album).to be_an_instance_of MusicAlbum
+    end
+  end
 end
-
